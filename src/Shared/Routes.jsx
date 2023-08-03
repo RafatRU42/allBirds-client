13545a435be3e6a991474328becf9982964d9f0b
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-08-03 11:08:57 +0600

    react router added

---
+++ src/Shared/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Main/MainLayout";
+import Home from "../Pages/Home";
+
+export const router = createBrowserRouter([
+    {path:'/',element:<MainLayout></MainLayout>,children:[
+        {path:'/',element:<Home></Home>}
+    ]}
+])
