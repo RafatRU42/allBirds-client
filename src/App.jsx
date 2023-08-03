d10ea0d302cc2c6a97212acfb7decd49d2cb3b72
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-08-03 10:03:54 +0600

    tailwind css installed

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <div>
+      <p>thsi is rafat</p>
+      <h1>My name is rafat</h1>
+
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+     </div>
+    </>
+  )
+}
+
+export default App
