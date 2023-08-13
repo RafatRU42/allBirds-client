1fe82ac1b16723f2bfe9c02dbec7a18ff9ad722c
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-08-13 12:38:25 +0600

    summer collection added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet";
 import ShowProduct from "./Products/ShowProduct";
 import Loading from "../Shared/Loading";
 import { Link } from "react-router-dom";
-import image from '../assets/architecture-2562316_640.jpg'
+import image from '../assets/download (6).jpg'
 
 const Home = () => {
   // const [product, setProduct] = useState([]);
@@ -267,6 +267,22 @@ const Home = () => {
 			</div>
 		</div>
 	</div>
+</section>
+
+{/* div 6 */}
+
+<section>
+	<div className="bg-error">
+		<div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
+			<h1 className="text-5xl font-bold leadi sm:text-6xl xl:max-w-3xl text-white font-link">Get Our Sessional legends collection.</h1>
+			<p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl!</p>
+			<div className="flex flex-wrap justify-center">
+				<button type="button" className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-gray-800 dark:text-gray-50">Branded Collection</button>
+				<button type="button" className="px-8 py-3 m-2 text-lg border rounded dark:border-gray-700 dark:text-gray-900">Premium Collection</button>
+			</div>
+		</div>
+	</div>
+	<img src='https://i.ibb.co/86MH5YT/pexels-photo-221387.jpg' alt="" className="w-5/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 dark:bg-gray-500" />
 </section>
 
     </div>
